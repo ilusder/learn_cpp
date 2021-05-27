8d04bc7a821c2aa452cebe4134d6cd987c5cd6e0
ilus <ilusder@gmail.com>
2021-05-27 21:47:45 +0300

    Add classes constructor

--- hw.cpp
+++ hw.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
+#include <string>
+using namespace std;
+
+class ball
+{
+    int id;
+    string name;
+    public:
+    ball(int id, string name)
+    {
+        this->id = id;
+        this->name = name;
+    }
+
+
+    void print_name(void)
+    {
+        cout << this->name << "\n";
+    }
+};
 
 int main (void)
 {
-    std::cout << "Hello World on C++\n";
+    cout << "Hello World on C++\n";
+    ball myball(1, "Red");
+    myball.print_name();
     return 0;
 }
 
